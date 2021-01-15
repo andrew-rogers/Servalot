536c10b6abd358bd986f3e9d5bba7057a29805dd
Andrew Rogers <andrew.rogerstech@gmail.com>
2021-01-15 15:29:57 +0000

    Moved connector thread class into StreamConnection.

--- app/src/main/java/uk/co/rogerstech/servalot/StreamConnection.java
+++ app/src/main/java/uk/co/rogerstech/servalot/StreamConnection.java
@@ -19,6 +19,10 @@
 
 package uk.co.rogerstech.servalot;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class StreamConnection {
 
     private Node node_a;
@@ -46,6 +50,40 @@ public class StreamConnection {
     public void close() {
         node_a.close();
         node_b.close();
+    }
+    
+    public class StreamConnectorThread extends Thread{
+
+        private InputStream is;
+        private OutputStream os;
+        private StreamConnection connection = null;
+
+        StreamConnectorThread(StreamConnection c, InputStream is, OutputStream os){
+            this.is=is;
+            this.os=os;
+            connection = c;
+        }
+
+        @Override
+        public void run() {
+            byte buffer[] = new byte[16*1024];
+            int nread;
+
+            try {
+
+                // Keep reading input until closed (or error).
+                while ((nread = is.read(buffer)) >= 0) {
+                    os.write(buffer, 0 ,nread);
+                }
+
+                // Input stream closed so close connection
+                connection.close();
+
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
