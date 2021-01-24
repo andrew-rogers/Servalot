4f1bfa6b168b480bef23df861e9966a2f279c3d1
Andrew Rogers <andrew.rogerstech@gmail.com>
2021-01-24 19:33:14 +0000

    Create Experimental applet for httpget test.

---
+++ example-packages/ServalotGUI/www/app.d/Experimental.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2021  Andrew Rogers
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
+var Experimental = function(servalot, gui) {
+    this.div_controls = gui.getAppControlsDiv();
+    this.div_main = gui.getMainDiv();
+    
+    // Create a get button
+    var btn_get = document.createElement("button");
+    btn_get.innerHTML = "Get";
+    this.div_main.appendChild(btn_get);
+
+    // For now, get file from fixed location when button is clicked.
+    btn_get.onclick=function() {
+        var url = 'https://github.com/andrew-rogers/andrew-rogers.github.io/raw/master/Servalot/Packages/HTTPDemo.zip';
+        var filename = 'thing.zip';
+        servalot.command({cmd: "httpget", url: url, filename: filename});
+    };
+};
+
