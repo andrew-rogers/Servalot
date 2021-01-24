b054523f9102d5a28cf988acd3291b31ce97ccdd
Andrew Rogers <andrew.rogerstech@gmail.com>
2021-01-24 20:52:03 +0000

    Move command line to Experimental applet.

--- example-packages/ServalotGUI/www/app.d/Experimental.js
+++ example-packages/ServalotGUI/www/app.d/Experimental.js
@@ -26,13 +26,16 @@
  */
 
 var Experimental = function(servalot, gui) {
+    this.servalot = servalot;
     this.div_controls = gui.getAppControlsDiv();
     this.div_main = gui.getMainDiv();
-    
+
     // Create a get button
     var btn_get = document.createElement("button");
     btn_get.innerHTML = "Get";
     this.div_main.appendChild(btn_get);
+
+    this.createCmdGUI();
 
     // For now, get file from fixed location when button is clicked.
     btn_get.onclick=function() {
@@ -42,3 +45,34 @@ var Experimental = function(servalot, gui) {
     };
 };
 
+Experimental.prototype.createCmdGUI = function() {
+
+    var div_run = document.createElement("div");
+    this.div_main.appendChild(div_run);
+
+    // Create textarea for command line
+    var ta_cmd = document.createElement("textarea");
+    ta_cmd.rows=1;
+    ta_cmd.style.width="80%";
+    div_run.appendChild(ta_cmd);
+
+    // Run button
+    var btn_run = document.createElement("button");
+    btn_run.innerHTML = "Run";
+    div_run.appendChild(btn_run);
+
+    // Create output area
+    var ta_output = document.createElement("textarea");
+    ta_output.rows=20;
+    ta_output.style.width="100%";
+    div_run.appendChild(ta_output);
+
+    // Handle click event
+    var that = this;
+    btn_run.onclick=function() {
+        that.servalot.command({cmd: "exec", args: ["sh", "-c", ta_cmd.value]}, function(obj) {
+            var str=atob(obj.stdout);
+            ta_output.value = ta_output.value + str + "\n";
+        });
+    };
+};
