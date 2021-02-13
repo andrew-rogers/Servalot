8470a9b5868e9a08ec68337d0a3a7a3680062135
Andrew Rogers <andrew.rogerstech@gmail.com>
2021-02-13 09:54:02 +0000

    Follow symbolic link chains to get correct WildBox applet name.

--- wildbox/wildbox-helper.sh
+++ wildbox/wildbox-helper.sh
@@ -6,7 +6,27 @@
 
 # Remove directory prefix. See Parameter Expansion section in bash man page.
 name="${1##*/}"
+name1="$1"
 shift
+
+# Follow any symbolic link chain to get applet name
+i=0
+while [ -L "$name1" ]; do
+    name="${name1##*/}"
+    dir1="${name1%/*}"
+    name1="$("$LD" "$BB" readlink "$name1")"
+
+    # If link target is not absolute path then prefix with directory of link
+    if [ "${name1:0:1}" != "/" ]; then
+        name1="$dir1/$name1"
+    fi
+
+    # Iterate no more than 20 times
+    i=$((i+1))
+    if [ $i -gt 20 ]; then
+        break
+    fi
+done
 
 # TODO: Eventually Servalot will be split into multiple APKS, search SERVALOT_PATH $name.so
 if [ -x "$SERVALOT_LIBS/$name.so" ]; then
